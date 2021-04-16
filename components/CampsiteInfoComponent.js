eaeb9686207e39aad34b5bc994bfafb551cf5be4
Gerald Warren <gwarren44102@outlook.com>
2021-04-15 21:59:49 -0400

    Redux Added

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,24 +1,74 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import { Card } from 'react-native-elements';
-import {CAMPSITES} from '../shared/campsites'
+import { Text, View, ScrollView, FlatList } from 'react-native';
+import { Card , Icon} from 'react-native-elements';
+import {CAMPSITES} from '../shared/campsites';
+import {COMMENTS} from '../shared/comments';
 
 
-function RenderCampsite({campsite}) {
+
+function RenderCampsite(props) {
+
+    const {campsite} = props;
+
+
     if (campsite) {
         return (
             <Card 
                 featuredTitle={campsite.name}
-                image={require('./images/react-lake.jpg')}
-            >
+                image={require('./images/react-lake.jpg')}>
                 <Text style={{margin: 10}}>
                     {campsite.description}
                 </Text>
+                <Icon 
+                    name={props.favorite ? 'heart' : 'heart-o'}
+                    type="font-awesome"
+                    color="#f50"
+                    raised
+                    reverse
+                    onPress={() => props.favorite ? 
+                        console.log('Already set as favorite') :
+                        props.markFavorite()}
+                
+                />
+                
             </Card>
         );
     }
     return <View />;
 }
+
+// CREATE THE RENDERCOMMENTS COMPONENT SO THAT WE CAN GENERATE THE COMPONENT
+// FIRST DESTRUCTURE THE ARRAY 
+// PLACE IT INTO A CARD COMPONENT 
+
+function RenderComments({comments}) {
+
+    // WE CAN SET UP A VIEW 
+    const renderCommentItem = ({item}) => {
+        return (
+            <View style={{margin: 10}}>
+                <Text style={{fontSize: 14}}>{item.text}</Text>
+                <Text style={{fontSize: 12}}>{item.rating}</Text>
+                <Text style={{fontSize: 12}}>{`-- ${item.author}, ${item.date}`}</Text>
+            </View>
+        )
+    }
+
+    return (
+        <FlatList 
+            data={comments}
+            renderItem={renderCommentItem}
+            // will setup the "renderCommentItem" function above
+            keyEctractor={item => item.id.toString()}
+        />
+    )
+
+}
+
+
+
+
+
 
 // COVERT FUNCTONAL TO CLASS COMPONENT
 class CampsiteInfo extends Component {
@@ -27,9 +77,18 @@ class CampsiteInfo extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            campsites: CAMPSITES
+            campsites: CAMPSITES,
+            comments: COMMENTS,
+            favorite: false
         };
     }
+
+    // CREATE HANDLER TO TOGGLE THE STATE "favorite" to true
+
+    markFavorite() {
+        this.setState({favorite: true});
+    }
+
 
     // AGAIN WE WILL SET THE TITLE FOR THE SCREEN 
     // Using the static method in the DirectoryNavigator
@@ -49,7 +108,25 @@ class CampsiteInfo extends Component {
         const campsite = this.state.campsites.filter(campsite => campsite.id === campsiteId)[0];
         // instead of props pass the campsite object we just created above
         //return <RenderCampsite campsite={props.campsite} />;
-        return <RenderCampsite campsite={campsite} />;
+
+        // FILTER OUT THE COMMENTS TO MATCH THE CAMPSITE ID
+        const comments = this.state.comments.filter(comment => comment.campsiteId === campsiteId);
+        
+        // WE WILL RENDER A NEW COMPONENT WITH THE COMMENTS THAT WE JUST FILTERED OUT
+        // AND WRAP IN A SCROLLVIEW
+        return (
+            <ScrollView>
+
+<RenderCampsite 
+    campsite={campsite} 
+    favorite={this.state.favorite} 
+    markFavorite={()=> this.markFavorite()} />
+
+<RenderComments comments={comments} /> 
+            </ScrollView>
+
+        )
+        
     }
     
 }
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react'
 import Directory from './DirectoryComponent'
 import CampsiteInfo from './CampsiteInfoComponent'
-import { View, Platform } from 'react-native'
+import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createAppContainer } from 'react-navigation'
 import Home from './HomeComponent'
-import { createDrawerNavigator } from 'react-navigation-drawer'
-import About from './AboutComponent';
-import Contact from './ContactComponent';
+import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer'
+import About from './AboutComponent'
+import Contact from './ContactComponent'
+import { Icon } from 'react-native-elements'
+import SafeAreaView from 'react-native-safe-area-view';
 
 // SETTING UP: WE ARE ONLY CREATING ONE STACK NAVIGATOR
 
 const DirectoryNavigator = createStackNavigator(
   {
     // As an arugment, we will set what components will be available for stack navigation
-    Directory: { screen: Directory },
+    Directory: {
+      screen: Directory,
+      navigationOptions: ({ navigation }) => ({
+        headerLeft: (
+          <Icon
+            name='list'
+            type='font-awesome'
+            iconStyle={styles.stackIcon}
+            onPress={() => navigation.toggleDrawer()}
+          />
+        )
+      })
+    },
     CampsiteInfo: { screen: CampsiteInfo }
   },
   {
@@ -35,114 +49,192 @@ const DirectoryNavigator = createStackNavigator(
 
 // Create a stack navigtor for Home page
 
-const HomeNavigator = createStackNavigator (
-
-    {
-        // As an arugment, we will set what components will be available for stack navigation
-        // ==> SET HOME COMPONENT AS ITS SCREEN
-        home: { screen: Home }
-        // THIS NAVIGATOR WILL ONLY HAVE 1 SCREEN SO WE CAN REMOVE CAMPSITEINFO
-        //CampsiteInfo: { screen: CampsiteInfo }
+const HomeNavigator = createStackNavigator(
+  {
+    // As an arugment, we will set what components will be available for stack navigation
+    // ==> SET HOME COMPONENT AS ITS SCREEN
+    home: { screen: Home }
+    // THIS NAVIGATOR WILL ONLY HAVE 1 SCREEN SO WE CAN REMOVE CAMPSITEINFO
+    //CampsiteInfo: { screen: CampsiteInfo }
+  },
+  {
+    // ADDITIONAL CONFIGURATION
+    // BECAUSE IT ONLY HAS ONE SCREEN WE WONT NEED 'initialRouteName'
+    //initialRouteName: 'Directory', // This will be the default on open
+    defaultNavigationOptions: ({ navigation }) => ({
+      // SET DEFAULT CONFIGURATIONS FOR HEADER
+      // NOTE: THERE ARE OTHER HEADERSTYLES AVAILABLE IF YOU WANT.
+      // SET BACKGROUND COLOR
+      headerStyle: {
+        backgroundColor: '#5637DD'
       },
-      {
-        // ADDITIONAL CONFIGURATION
-        // BECAUSE IT ONLY HAS ONE SCREEN WE WONT NEED 'initialRouteName'
-        //initialRouteName: 'Directory', // This will be the default on open
-        defaultNavigationOptions: {
-          // SET DEFAULT CONFIGURATIONS FOR HEADER
-          // NOTE: THERE ARE OTHER HEADERSTYLES AVAILABLE IF YOU WANT.
-          // SET BACKGROUND COLOR
-          headerStyle: {
-            backgroundColor: '#5637DD'
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: { color: '#fff' }
-        }
-      }
+      headerTintColor: '#fff',
+      headerTitleStyle: { color: '#fff' },
 
+      headerLeft: (
+        <Icon
+          name='home'
+          type='font-awesome'
+          iconStyle={styles.stackIcon}
+          onPress={() => navigation.toggleDrawer()}
+        />
+      )
+    })
+  }
 )
 
-
-const ContactNavigator = createStackNavigator (
-
+const ContactNavigator = createStackNavigator(
   {
-      // As an arugment, we will set what components will be available for stack navigation
-      // ==> SET HOME COMPONENT AS ITS SCREEN
-      contact: { screen: Contact }
-      // THIS NAVIGATOR WILL ONLY HAVE 1 SCREEN SO WE CAN REMOVE CAMPSITEINFO
-      //CampsiteInfo: { screen: CampsiteInfo }
-    },
-    {
-      // ADDITIONAL CONFIGURATION
-      // BECAUSE IT ONLY HAS ONE SCREEN WE WONT NEED 'initialRouteName'
-      //initialRouteName: 'Directory', // This will be the default on open
-      defaultNavigationOptions: {
-        // SET DEFAULT CONFIGURATIONS FOR HEADER
-        // NOTE: THERE ARE OTHER HEADERSTYLES AVAILABLE IF YOU WANT.
-        // SET BACKGROUND COLOR
-        headerStyle: {
-          backgroundColor: '#5637DD'
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: { color: '#fff' }
-      }
-    }
-
-)
-
-
-
-const AboutNavigator = createStackNavigator (
-
+    // As an arugment, we will set what components will be available for stack navigation
+    // ==> SET HOME COMPONENT AS ITS SCREEN
+    contact: { screen: Contact }
+    // THIS NAVIGATOR WILL ONLY HAVE 1 SCREEN SO WE CAN REMOVE CAMPSITEINFO
+    //CampsiteInfo: { screen: CampsiteInfo }
+  },
   {
-      // As an arugment, we will set what components will be available for stack navigation
-      // ==> SET HOME COMPONENT AS ITS SCREEN
-      about: { screen: About }
-      // THIS NAVIGATOR WILL ONLY HAVE 1 SCREEN SO WE CAN REMOVE CAMPSITEINFO
-      //CampsiteInfo: { screen: CampsiteInfo }
-    },
-    {
-      // ADDITIONAL CONFIGURATION
-      // BECAUSE IT ONLY HAS ONE SCREEN WE WONT NEED 'initialRouteName'
-      //initialRouteName: 'Directory', // This will be the default on open
-      defaultNavigationOptions: {
-        // SET DEFAULT CONFIGURATIONS FOR HEADER
-        // NOTE: THERE ARE OTHER HEADERSTYLES AVAILABLE IF YOU WANT.
-        // SET BACKGROUND COLOR
-        headerStyle: {
-          backgroundColor: '#5637DD'
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: { color: '#fff' }
-      }
-    }
+    // ADDITIONAL CONFIGURATION
+    // BECAUSE IT ONLY HAS ONE SCREEN WE WONT NEED 'initialRouteName'
+    //initialRouteName: 'Directory', // This will be the default on open
+    defaultNavigationOptions: ({ navigation }) => ({
+      // SET DEFAULT CONFIGURATIONS FOR HEADER
+      // NOTE: THERE ARE OTHER HEADERSTYLES AVAILABLE IF YOU WANT.
+      // SET BACKGROUND COLOR
+      headerStyle: {
+        backgroundColor: '#5637DD'
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: { color: '#fff' },
 
+      headerLeft: (
+        <Icon
+          name='address-card'
+          type='font-awesome'
+          iconStyle={styles.stackIcon}
+          onPress={() => navigation.toggleDrawer()}
+        />
+      )
+    })
+  }
 )
+
+const AboutNavigator = createStackNavigator(
+  {
+    // As an arugment, we will set what components will be available for stack navigation
+    // ==> SET HOME COMPONENT AS ITS SCREEN
+    about: { screen: About }
+    // THIS NAVIGATOR WILL ONLY HAVE 1 SCREEN SO WE CAN REMOVE CAMPSITEINFO
+    //CampsiteInfo: { screen: CampsiteInfo }
+  },
+  {
+    // ADDITIONAL CONFIGURATION
+    // BECAUSE IT ONLY HAS ONE SCREEN WE WONT NEED 'initialRouteName'
+    //initialRouteName: 'Directory', // This will be the default on open
+    defaultNavigationOptions: ({ navigation }) => ({
+      // SET DEFAULT CONFIGURATIONS FOR HEADER
+      // NOTE: THERE ARE OTHER HEADERSTYLES AVAILABLE IF YOU WANT.
+      // SET BACKGROUND COLOR
+      headerStyle: {
+        backgroundColor: '#5637DD'
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: { color: '#fff' },
+
+      headerLeft: (
+        <Icon
+          name='info-circle'
+          type='font-awesome'
+          iconStyle={styles.stackIcon}
+          onPress={() => navigation.toggleDrawer()}
+        />
+      )
+    })
+  }
+)
+
+
+const CustomDrawerContentComponent = props => (
+  <ScrollView>
+      <SafeAreaView 
+          style={styles.container}
+          forceInset={{top: 'always', horizontal: 'never'}}>
+          <View style={styles.drawerHeader}>
+              <View style={{flex: 1}}>
+                  <Image source={require('./images/logo.png')} style={styles.drawerImage} />
+              </View>
+              <View style={{flex: 2}}>
+                  <Text style={styles.drawerHeaderText}>NuCamp</Text>
+              </View>
+          </View>
+          <DrawerItems {...props} />
+      </SafeAreaView>
+  </ScrollView>
+);
 
 
 
 // ADD THE DRAWER NAVIGATOR
-const MainNavigator = createDrawerNavigator (
-
-    // FIRST CREATE THE SCREENS THAT WILL BE IN THE DRAWER
-    // WE WILL ADD THE HOME AND DIRECTORY SCREENS IN THIS DRAWER
-    // YOU WILL WANT TO ROUTE THEM THROUGH THE STACK NAVIGATOR ... SO WE WILL GIVE
-    // THEM THE HOME AND DIRECTORY NAVIGATORS INSTEAD OF THE COMPONENTS THEMSELVES
-    {
-        Home: { screen: HomeNavigator},
-        Directory: { screen: DirectoryNavigator },
-        Contact: { screen: ContactNavigator },
-        About: { screen: AboutNavigator },
-    },
-    {
-        // ADDING OPTIONAL SECOND ARGUMENT FOR ADDTIONAL CONFIGURATIONS
-        drawerBackgroundColor: '#CEC8FF'
-    }
-
-
-)
-
-
+const MainNavigator = createDrawerNavigator(
+  {
+      Home: {
+          screen: HomeNavigator,
+          navigationOptions: {
+              drawerIcon: ({tintColor}) => (
+                  <Icon
+                      name='home'
+                      type='font-awesome'
+                      size={24}
+                      color={tintColor}
+                  />
+              )
+          }
+      },
+      Directory: {
+          screen: DirectoryNavigator,
+          navigationOptions: {
+              drawerIcon: ({tintColor}) => (
+                  <Icon
+                      name='list'
+                      type='font-awesome'
+                      size={24}
+                      color={tintColor}
+                  />
+              )
+          }
+      },
+      About: {
+          screen: AboutNavigator,
+          navigationOptions: {
+              drawerLabel: 'About Us',
+              drawerIcon: ({tintColor}) => (
+                  <Icon
+                      name='info-circle'
+                      type='font-awesome'
+                      size={24}
+                      color={tintColor}
+                  />
+              )
+          }
+      },
+      Contact: {
+          screen: ContactNavigator,
+          navigationOptions: {
+              drawerLabel: 'Contact Us',
+              drawerIcon: ({tintColor}) => (
+                  <Icon
+                      name='address-card'
+                      type='font-awesome'
+                      size={24}
+                      color={tintColor}
+                  />
+              )
+          }
+      }
+  },
+  {
+      drawerBackgroundColor: '#CEC8FF',
+      contentComponent: CustomDrawerContentComponent
+  }
+);
 
 // NOW THAT YOU CRATED A STACK NAVIGATOR, WE NEED TO PASS IT TO A FUNCTION
 // "createAppContainer" will return a component.
@@ -164,10 +256,12 @@ class Main extends Component {
 
   render () {
     return (
-      <View style={{ 
+      <View
+        style={{
           flex: 1,
           paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight
-          }}>
+        }}
+      >
         {/* // Contains Directory and CampsiteInfo components */}
         <AppNavigator />
         {/* // REPLACED WITH AppNavigator */}
@@ -183,5 +277,33 @@ class Main extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+},
+drawerHeader: {
+    backgroundColor: '#5637DD',
+    height: 140,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    flexDirection: 'row'
+},
+drawerHeaderText: {
+    color: '#fff',
+    fontWeight: 'bold'
+},
+drawerImage: {
+    margin: 10,
+    height: 60,
+    width: 60
+},
+  stackIcon: {
+    marginLeft: 10,
+    color: '#fff',
+    fontSize: 24
+  }
+})
 
 export default Main
